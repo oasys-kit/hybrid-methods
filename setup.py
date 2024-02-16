df40b5fe44f7f198572400919e972de8e601126f
lrebuffi <lrebuffi@anl.gov>
2024-02-15 18:49:24 -0600

    s4hybrid to python script Hybrid for KB

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except AttributeError:
 
 NAME = 'shadow-hybrid-methods'
 
-VERSION = '1.0.2'
+VERSION = '1.0.3'
 ISRELEASED = True
 
 DESCRIPTION = 'Hybrid Methods, combining raytracing with wave optics'
